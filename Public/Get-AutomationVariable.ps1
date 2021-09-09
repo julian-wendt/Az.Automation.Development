7ee685def5fcf77f807f53c284f7d70643e3dbe3
Julian Wendt <11994116+julian-wendt@users.noreply.github.com>
2021-09-09 15:46:43 +0200

    Add new line breaks to function description

--- Public/Get-AutomationVariable.ps1
+++ Public/Get-AutomationVariable.ps1
@@ -4,8 +4,9 @@ function Get-AutomationVariable {
     Emulates "Get-AutomationVariable" from Azure Automation Account.
     
     .DESCRIPTION
-    Intended for Azure Automation Runbook development on local machines, this function emulates the built-in Automation function "Get-AutomationVariable". Therefore the "Microsoft.PowerShell.SecretManagement" module
-    is used to safely return sensitive information like secrets or keys. To store them, simply use the Set-Secret command". Less sensitive information can also be stored next to the environment details in the settings file.
+    Intended for Azure Automation Runbook development on local machines, this function emulates the built-in Automation function "Get-AutomationVariable".
+    Therefore the "Microsoft.PowerShell.SecretManagement" module is used to safely return sensitive information like secrets or keys. To store them, simply
+    use the Set-Secret command". Less sensitive information can also be stored next to the environment details in the settings file.
 
     Set-Secret: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.secretmanagement/set-secret?view=ps-modules
 
